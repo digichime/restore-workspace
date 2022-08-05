aaf8f84a74dfa54a4a4ef7d0f4a9bf045aaff896
Yudai Shinnoki <y.shinnoki@gmail.com>
2022-08-06 07:01:16 +0900

    ➖ js-yamlの依存削除

--- index.ts
+++ index.ts
@@ -1,49 +1,65 @@
 import * as fs from "fs";
-import { load } from "js-yaml";
+import * as readline from "readline";
 
-type YarnLockItem = {
-  version: string;
-  resolution: string;
-  dependencies?: {
-    [key: string]: string;
-  };
-  peerDependencies?: {
-    [key: string]: string;
-  };
-  peerDependenciesMeta?: {
-    [key: string]: { optional: boolean };
-  };
-  bin?: {
-    [key: string]: string;
-  };
-  checksum?: string;
-  languageName: string;
-  linkType: string;
+const parseLine = (line: string) => {
+  const [key, ...rest] = line.replaceAll('"', "").split(":");
+  return [key.trim(), rest.join(":").trim()];
 };
 
-Object.entries(load(fs.readFileSync("./yarn.lock", "utf-8")) as any)
-  .filter(([key]) => key !== "__metadata")
-  .map(([, value]) => value as YarnLockItem)
-  .filter(({ resolution }) => resolution.includes("@workspace:"))
-  .forEach(({ resolution, dependencies, peerDependencies, peerDependenciesMeta, bin }) => {
-    const [name, packageDir] = resolution.split("@workspace:");
-
-    // Skip root package.json
-    if (packageDir === ".") return;
-
-    const packageJsonPath = packageDir + "/package.json";
-
-    // Do nothing if the package.json already exists
-    if (fs.existsSync(packageJsonPath)) return;
-
-    // Create package directory if not exists
-    if (!fs.existsSync(packageDir)) {
-      fs.mkdirSync(packageDir, { recursive: true });
-    }
-
-    // Create package.json
-    fs.writeFileSync(
-      packageJsonPath,
-      JSON.stringify({ name, dependencies, peerDependencies, peerDependenciesMeta, bin })
-    );
+(async () => {
+  const stream = fs.createReadStream("./yarn.lock", "utf-8");
+  const rl = readline.createInterface({
+    input: stream,
   });
+
+  let packageJson: { dir: string; value: any } | null = null;
+  let field: { name: string; value: any } | null = null;
+
+  for await (const line of rl) {
+    if (line.startsWith("    ")) {
+      // 4 spaces indent
+      if (field) {
+        const [name, value] = parseLine(line);
+        field.value[name] = value;
+      }
+    } else if (line.startsWith("  ")) {
+      // 2 spaces indent
+      if (packageJson && field) {
+        packageJson.value[field.name] = field.value;
+        field = null;
+      }
+      const [name, value] = parseLine(line);
+      if (name === "resolution") {
+        if (value.includes("@workspace:")) {
+          const [packageName, packageDir] = value.split("@workspace:");
+          if (packageDir !== ".") {
+            packageJson = { dir: packageDir, value: { name: packageName } };
+          }
+        }
+      } else if (
+        name === "dependencies" ||
+        name === "peerDependencies" ||
+        name === "peerDependenciesMeta" ||
+        name === "bin"
+      ) {
+        field = { name, value: {} };
+      }
+    } else {
+      // No indent
+      if (packageJson) {
+        // Make sure the package directory  exists
+        if (!fs.existsSync(packageJson.dir)) {
+          fs.mkdirSync(packageJson.dir, { recursive: true });
+        }
+
+        // Create the package.json if not exists
+        const packageJsonPath = packageJson.dir + "/package.json";
+        if (!fs.existsSync(packageJsonPath)) {
+          fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson.value));
+        }
+
+        packageJson = null;
+      }
+    }
+  }
+})();
